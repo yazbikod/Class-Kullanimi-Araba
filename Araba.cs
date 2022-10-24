d43f7469f3b58b502d8f1eb953660feb34ab7f40
yazbikod <aslan_murathan@hotmail.com>
2022-10-25 01:37:59 +0300

    Proje dosyası ekle.

---
+++ Araba.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Class_Kullanımı
+{
+    public class Araba
+    {
+        public string arabarengi;
+
+        public int arabamodeli;
+
+        public Araba(string _arabarengi, int _arabamodeli)
+        {
+            arabarengi = _arabarengi;
+            arabamodeli = _arabamodeli;
+
+        }
+
+        public void ozellik()
+        {
+            Console.WriteLine("Araba rengi : " + arabarengi);
+            Console.WriteLine("Araba Modeli : " + arabamodeli);
+        }
+
+        public void Motorcalistir()
+        {
+            Console.WriteLine("Motor çalışıyor.....");
+        }
+
+
+
+    }
+
+
+}
---
+++ Program.cs
@@ -0,0 +1,12 @@
+﻿using Class_Kullanımı;
+
+Araba tortor = new Araba("Kırmızı", 1986);
+Console.WriteLine("Uygulamamıza Hoşgeldiniz...");
+
+Console.WriteLine(tortor.arabarengi);
+Console.WriteLine(tortor.arabamodeli);
+
+
+tortor.ozellik();
+tortor.Motorcalistir();
+
